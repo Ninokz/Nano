b5b04b3fff1e085aef645a200ed4b1f9bd0f7141
Ninokz <2606904530@qq.com>
2024-09-11 01:23:41 +0800

    添加 RPCServer 类

--- Nano/rpcserver.h
+++ Nano/rpcserver.h
@@ -1,11 +1,14 @@
 #pragma once
-
+#include <memory>
+#include <string>
+#include <unordered_map>
 
 #include "BaseServer.h"
 
 namespace Nano {
 	namespace Rpc {
-		class RPCServer {
+		class RPCServer : public Nano::Communication::BaseServer
+		{
 
 		};
 	}
